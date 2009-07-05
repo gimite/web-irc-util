e38d7e54e920ec4802af7b32fecf1d69c6419684
Hiroshi Ichikawa <gimite@gmail.com>
2009-07-05 11:12:46 +0900

    Adding Google Analytics tracker.

--- www/app/controllers/application.rb
+++ www/app/controllers/application.rb
@@ -1,2 +1,9 @@
 class Application < Merb::Controller
+    
+    if File.exist?("config/google_analytics.txt")
+      GOOGLE_ANALYTICS_ID = File.read("config/google_analytics.txt").strip()
+    else
+      GOOGLE_ANALYTICS_ID = nil
+    end
+    
 end
